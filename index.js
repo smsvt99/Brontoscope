3b82d4f8c968385cb2ca117f5d77164d5283d236
Sean Stone <smstone@uvm.edu>
2018-10-10 19:08:24 -0400

    spelling correction

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-const port = provess.end.PORT
+const port = process.end.PORT
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
