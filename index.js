329fba28ac36c36fcdd39b297507ea077bd0208e
Sean Stone <smstone@uvm.edu>
2018-10-10 19:01:49 -0400

    remove unsed script tag

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-const port = process.env.PORT
+const port = 5000
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
