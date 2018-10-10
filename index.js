3edf1f6518c7508db70999af7b54db53d1aa630f
Sean Stone <smstone@uvm.edu>
2018-10-10 17:31:46 -0400

    process.env.PORT

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-const port = 5003
+const port = process.env.PORT
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
